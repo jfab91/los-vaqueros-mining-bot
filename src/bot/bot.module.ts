e862d30a5be8bd8976cd4013a41282a0906f095c
Fabricio Vargas <fabricio.vargas@gamanza.com>
2021-05-24 16:29:48 -0600

    remove endpoint for send messages directly

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BotProvider } from './providers/bot.provider';
-import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
 
 @Module({
   providers: [BotProvider, BotService],
-  controllers: [BotController],
   exports: [BotService],
 })
 export class BotModule {}
