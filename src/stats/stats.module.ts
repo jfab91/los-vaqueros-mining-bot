6d68f9b8dfde4a64662f779054afc38dd90bd3f0
Fabricio Vargas <fabricio.vargas@gamanza.com>
2021-05-29 14:22:25 -0600

    remove unused stats controller

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { JobsService } from './services/jobs.service';
 import { StatsService } from './services/stats.service';
 import { BotModule } from '../bot/bot.module';
-import { StatsController } from './stats.controller';
 
 @Module({
   imports: [BotModule],
   providers: [JobsService, StatsService],
-  controllers: [StatsController]
 })
 export class StatsModule {}
