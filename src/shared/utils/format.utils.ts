47b17f161b961bf3bb822b0fb11743c31a2cd983
Fabricio Vargas <fabricio.vargas@gamanza.com>
2021-05-29 14:23:11 -0600

    Update format.utils.ts

--- src/shared/utils/format.utils.ts
+++ src/shared/utils/format.utils.ts
@@ -1,9 +1,17 @@
 export class FormatUtils {
-  public static formatHashRate(hashrate: number): string {
-    return `${(hashrate / 1000000).toFixed(2)} MH/s`;
+  public static formatHashRate(hashrate: number): number {
+    return Number((hashrate / 1000000).toFixed(2));
   }
 
   public static convertTimeStampToDate(timestamp: number): string {
     return new Date(timestamp * 1000).toLocaleString()
+  }
+
+  public static formatEthBalance(balance: number): number {
+    return Number((balance / 1000000000000000000).toFixed(6));
+  }
+
+  public static formatUSDBalance(balance: number, price: number): number {
+    return Number((balance * price).toFixed(2));
   }
 }
