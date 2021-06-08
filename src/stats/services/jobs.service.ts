e52da0694a75e7f06dfad3b45a814eef4c676c12
Fabricio Vargas <fabricio.vargas@gamanza.com>
2021-06-07 23:09:07 -0600

    update job service

--- src/stats/services/jobs.service.ts
+++ src/stats/services/jobs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { WORKER_NAMES } from '../../shared/constants';
 import { Cron, CronExpression } from '@nestjs/schedule';
@@ -6,6 +6,11 @@ import { OfflineTemplate } from '../../shared/classes/offline-template';
 import { BotService } from '../../bot/bot.service';
 import { DailyReportTemplate } from '../../shared/classes/daily-report-template';
 import { RigBalanceInfo } from '../../shared/interfaces/rig-balance-info.interface';
+import { MESSAGE_COUNTER_PROVIDER } from '../stats-constants';
+import { MessageCounter } from '../classes/message-counter';
+import { ResumedWorkerStatus } from '../../shared/interfaces/resumed-worker-status.interface';
+import { ICounter } from '../interfaces/counter.interface';
+import { BackToOnlineTemplate } from '../../shared/classes/back-to-online-template';
 
 @Injectable()
 export class JobsService {
@@ -13,7 +18,8 @@ export class JobsService {
 
   constructor(
     private readonly statsService: StatsService,
-    private readonly bot: BotService
+    private readonly bot: BotService,
+    @Inject(MESSAGE_COUNTER_PROVIDER) private readonly messageCounter: MessageCounter,
   ) {}
   
   /**
@@ -39,13 +45,32 @@ export class JobsService {
    */
   @Cron(CronExpression.EVERY_MINUTE)
   async checkOnlineStatus(): Promise<void> {
+    const { offline } = this.messageCounter;
+
     for (const name of WORKER_NAMES) {
       const worker = await this.statsService.getWorkerStatus(name);
 
-      if (worker && !worker.online) {
-        this.bot.send(new OfflineTemplate({ name: worker.name, lastSeen: worker.lastSeen}).getMessage());
-        this.logger.debug(`worker ${name} is offline!!`);
+      if (worker) {
+        if (!worker.online) {
+          this.sendOfflineMessage(worker, offline);
+          this.logger.debug(`worker ${name} is offline!!`);
+        }
+
+        this.sendBackToOnlineMessage(worker, offline);
       }
+    }
+  }
+
+  private sendOfflineMessage(worker: ResumedWorkerStatus, counter: ICounter) {
+    if (!counter.reachedMax()) {
+      this.bot.send(new OfflineTemplate({ workerName: worker.name, lastSeen: worker.lastSeen}).getMessage());
+    }
+  }
+
+  private sendBackToOnlineMessage(worker: ResumedWorkerStatus, counter: ICounter) {
+    if (counter.reachedMax()) {
+      this.bot.send(new BackToOnlineTemplate({ workerName: worker.name }).getMessage());
+      counter.reset();
     }
   }
 }
