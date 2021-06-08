65756f9f7bdf2e6aa1aaa6a2400617dab2794c06
Fabricio Vargas <fabricio.vargas@gamanza.com>
2021-06-07 23:08:49 -0600

    add new provider for message counter

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { JobsService } from './services/jobs.service';
 import { StatsService } from './services/stats.service';
 import { BotModule } from '../bot/bot.module';
+import { MessageCounter } from './classes/message-counter';
+import { MESSAGE_COUNTER_PROVIDER } from './stats-constants';
 
 @Module({
   imports: [BotModule],
-  providers: [JobsService, StatsService],
+  providers: [
+    JobsService, 
+    StatsService,
+    {
+      provide: MESSAGE_COUNTER_PROVIDER,
+      useClass: MessageCounter,
+    },
+  ],
 })
 export class StatsModule {}
